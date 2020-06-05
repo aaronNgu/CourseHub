08f57ce1c532daafbc1db4cfb733a556ba9924ff
Aaron <Aaron@As-MacBook-Pro.local>
2020-06-05 16:09:19 -0700

    moved css into index.css

--- src/components/CourseItem.js
+++ src/components/CourseItem.js
@@ -1,41 +1,20 @@
 import React from 'react';
 
-class CourseItem extends React.Component{
+class CourseItem extends React.Component {
     render() {
-        const styles = {
-            courseitem: {
-                display: 'flex',
-                flexDirection: 'column',
-                border: 'solid',
-                width: '30%',
-                padding: '10px;'
-            },
-            courseitem_top: {
-                display: 'flex'
-            },
-            courseitem_bottom: {
-                display: 'flex'
-            },
-            courseitem_review: {
-                border: "solid",
-                marginLeft: "10px",
-                padding: "5px"
-            }
-        }
-
         return (
-            <div style={styles.courseitem} > 
+            <div className="courseitem" >
 
-                <div style={styles.courseitem_top}>
-                    <h3>CPSC 110</h3> 
+                <div className="courseitem_top">
+                    <h3>CPSC 110</h3>
                     &nbsp; &nbsp;
                     <h3>Computation, Programs and Programming</h3>
                 </div>
-                <div style={styles.courseitem_bottom}>
+                <div className="courseitem_bottom">
                     <div><h4>Ratings 4/5</h4></div>
-                    <div style={styles.courseitem_review}><p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Aenean sodales rutrum sagittis. Integer ipsum nisi, 
-                        sodales euismod pretium eget, fermentum eget arcu. 
+                    <div className="courseitem_review"><p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Aenean sodales rutrum sagittis. Integer ipsum nisi,
+                    sodales euismod pretium eget, fermentum eget arcu.
                         Phasellus scelerisque tellus porttitor sem rutrum, pl...” more</p></div>
                 </div>
             </div>
