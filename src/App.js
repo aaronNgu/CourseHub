e3a7db490c471a368aff5911a38ced0120d39861
sbaruah <spandana.baruah@gmail.com>
2020-06-05 07:51:34 -0700

    adding the whole project folder plus button and header component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from './Button';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
