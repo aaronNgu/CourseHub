25a4584cdce96058922f091fb393466293072ae8
Illean Zhang <illean@outlook.com>
2020-06-04 23:05:03 -0700

    added some styling loosely based on figma

--- src/components/ReviewItem.js
+++ src/components/ReviewItem.js
@@ -6,12 +6,12 @@ class ReviewItem extends React.Component {
         return (
             <div className="reviewItem">
                 <div className="ratingSummary">
-                    <div className="rating">
-                        <h3 className="dates">Rating: {this.props.rating}/5</h3>
+                    <div>
+                        <h3 className="rating">Rating: {this.props.rating}/5</h3>
                     </div>
                     <div className="thumbButtons">
-                        <button><i class="material-icons md-24">thumb_up</i></button>
-                        <button><i class="material-icons md-24">thumb_down</i></button>
+                        <button><i class="material-icons">thumb_up</i></button>
+                        <button><i class="material-icons">thumb_down</i></button>
                     </div>
                 </div>
                 <div className="review">
