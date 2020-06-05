9aa02c23091286a7e0b7e782e76b339997b77e51
wchan <Because1215>
2020-06-05 16:28:38 -0400

    Added courseOverview

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import ReviewItemList from "./components/ReviewItemList";
 import CourseItem from './components/CourseItem';
+import CourseOverview from './components/courseOverview';
 
 const App = () => {
   return (
     <div className="App">
       <ReviewItemList />
       <CourseItem />
+      <CourseOverview />
     </div>
   );
 };
--- src/components/courseOverview.js
+++ src/components/courseOverview.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React from 'react';
 
-class courseOverview extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
+class CourseOverview extends React.Component{
   render() {
-    let course: this.props.course
-    return (
-      <div>
-      <p>Overall Rating</p>
-      <p>{course.rating}</p>
-      <p>{course.code}</p>
-        <h1>{course.name}</h1>
-        <p>{course.description}</p>
-      </div>
-    );
+      const styles = {
+          courseitem: {
+              display: 'flex',
+              flexDirection: 'column',
+              border: 'solid',
+              width: '30%',
+              padding: '10px;'
+          },
+          courseitem_top: {
+              display: 'flex'
+          },
+          courseitem_bottom: {
+              display: 'flex'
+          },
+          courseitem_review: {
+              border: "solid",
+              marginLeft: "10px",
+              padding: "5px"
+          }
+      }
+
+      return (
+          <div style={styles.courseitem} >
+
+              <div style={styles.courseitem_top}>
+                  <h3>CPSC 110</h3>
+                  &nbsp; &nbsp;
+                  <h3>Computation, Programs and Programming</h3>
+              </div>
+              <div style={styles.courseitem_bottom}>
+                  <div><h4>Overall Rating 4/5</h4></div>
+                  <div style={styles.courseitem_review}><p>Fundamental program and computation structures. Introductory programming skills. Computation as a tool for information processing, simulation and modelling, and interacting with the world.</p></div>
+              </div>
+          </div>
+      );
   }
 }
 
-const mapStateToProps = (state) => {
-return { Course: state.course };
-}
-
-export default connect(mapStateToProps, {courseItem })(courseOverview);
+export default CourseOverview;
