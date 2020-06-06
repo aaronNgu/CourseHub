b2536a7843e6ce7bcd548a0a4877ca24da1e74f5
sbaruah <spandana.baruah@gmail.com>
2020-06-05 22:39:51 -0700

    remove extra review-item-list component from app render

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ const App = () => {
     <div className="App">
       <Header />
       <Button />
-      <ReviewItemList />
       <CourseOverview />
       <CourseItem />
       <ReviewItemList />
