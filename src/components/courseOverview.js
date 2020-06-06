f2d7c1bfbe1cb04ae1159a59e8b16062fceb9f69
wchan <Because1215>
2020-06-05 20:17:32 -0400

    Rebased courseOverview

--- src/components/courseOverview.js
+++ src/components/courseOverview.js
@@ -2,38 +2,17 @@ import React from 'react';
 
 class CourseOverview extends React.Component{
   render() {
-      const styles = {
-          courseitem: {
-              display: 'flex',
-              flexDirection: 'column',
-              border: 'solid',
-              width: '30%',
-              padding: '10px;'
-          },
-          courseitem_top: {
-              display: 'flex'
-          },
-          courseitem_bottom: {
-              display: 'flex'
-          },
-          courseitem_review: {
-              border: "solid",
-              marginLeft: "10px",
-              padding: "5px"
-          }
-      }
-
       return (
-          <div style={styles.courseitem} >
+          <div className="courseOverview" >
 
-              <div style={styles.courseitem_top}>
+              <div>
                   <h3>CPSC 110</h3>
                   &nbsp; &nbsp;
                   <h3>Computation, Programs and Programming</h3>
               </div>
-              <div style={styles.courseitem_bottom}>
-                  <div><h4>Overall Rating 4/5</h4></div>
-                  <div style={styles.courseitem_review}><p>Fundamental program and computation structures. Introductory programming skills. Computation as a tool for information processing, simulation and modelling, and interacting with the world.</p></div>
+              <div>
+                  <div className="rating"><h3>Overall Rating 4/5</h3></div>
+                  <div className="reviewText"><p>Fundamental program and computation structures. Introductory programming skills. Computation as a tool for information processing, simulation and modelling, and interacting with the world.</p></div>
               </div>
           </div>
       );
