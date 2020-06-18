91e491dde0e425652aa3a9a1e6cc6d73b01a296a
Aaron <Aaron@As-MacBook-Pro.local>
2020-06-18 12:15:36 -0700

    courseItem completed

--- src/components/CourseItem.js
+++ src/components/CourseItem.js
@@ -1,25 +1,48 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography, Paper, Box} from '@material-ui/core';
 
-class CourseItem extends React.Component {
-    render() {
-        return (
-            <div className="courseitem" >
-
-                <div className="courseitem_top">
-                    <h3>CPSC 110</h3>
-                    &nbsp; &nbsp;
-                    <h3>Computation, Programs and Programming</h3>
-                </div>
-                <div className="courseitem_bottom">
-                    <div><h4>Ratings 4/5</h4></div>
-                    <div className="courseitem_review"><p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Aenean sodales rutrum sagittis. Integer ipsum nisi,
-                    sodales euismod pretium eget, fermentum eget arcu.
-                        Phasellus scelerisque tellus porttitor sem rutrum, pl...” more</p></div>
-                </div>
-            </div>
-        );
+const useStyles = makeStyles({
+    rating: {
+        padding: '0px 22px',
+        marginBottom: '5px',
+        backgroundColor: '#E5A0A0',
+    }, 
+    main: {
+        width: '800px',
+        backgroundColor: '#FFECEC',
+        ['@media (max-width:900px)']: {
+            width: '100%',
+        }
     }
+})
+
+function CourseItem (props) {
+    const classes = useStyles();
+
+    return <Box className={ `courseItemHorizontal ${ classes.main }` }>
+
+            <Box className='courseItemHorizontal'>
+
+                <Box className='courseItemVerticalSides'>
+                    <Typography variant='h6'>Rating</Typography>
+                    <Paper className={ classes.rating }>
+                        <Typography variant='h3'>{ props.rating || 5 }</Typography>
+                    </Paper>
+                </Box>
+
+                <Box className='courseItemVerticalSides courseItemVerticalMiddle'>
+                    <Typography variant='h5'>{ props.courseNumber || 'CPSC110' } </Typography>
+                    <Typography variant='body2'>{ props.review || 'It was an awsome course!' }</Typography>
+                </Box>
+
+            </Box>
+
+            <Box className='courseItemVerticalSides courseItemVerticalMiddle'>
+                <Typography variant='body2'>{ props.date || '2/19/2020' }</Typography>
+            </Box>
+
+        </Box>;
 }
 
 export default CourseItem;
