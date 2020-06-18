7b382136df5dbf5b165fcc6c0c315c530efca295
wchan <Because1215>
2020-06-17 23:44:44 -0400

    Reverted CourseItem changes

--- src/components/CourseItem.js
+++ src/components/CourseItem.js
@@ -1,25 +1,25 @@
 import React from 'react';
 
 class CourseItem extends React.Component {
-  render() {
-      return (
-          <div className="courseitem" >
+    render() {
+        return (
+            <div className="courseitem" >
 
-              <div className="courseitem_top">
-                  <h3>CPSC 110</h3>
-                  &nbsp; &nbsp;
-                  <h3>Computation, Programs and Programming</h3>
-              </div>
-              <div className="courseitem_bottom">
-                  <div><h4>Ratings 4/5</h4></div>
-                  <div className="courseitem_review"><p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Aenean sodales rutrum sagittis. Integer ipsum nisi,
-                  sodales euismod pretium eget, fermentum eget arcu.
-                      Phasellus scelerisque tellus porttitor sem rutrum, pl...” more</p></div>
-              </div>
-          </div>
-      );
-  }
+                <div className="courseitem_top">
+                    <h3>CPSC 110</h3>
+                    &nbsp; &nbsp;
+                    <h3>Computation, Programs and Programming</h3>
+                </div>
+                <div className="courseitem_bottom">
+                    <div><h4>Ratings 4/5</h4></div>
+                    <div className="courseitem_review"><p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Aenean sodales rutrum sagittis. Integer ipsum nisi,
+                    sodales euismod pretium eget, fermentum eget arcu.
+                        Phasellus scelerisque tellus porttitor sem rutrum, pl...” more</p></div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default CourseItem;
