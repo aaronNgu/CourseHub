9f87540a130f5486970fa7911d69984bf65fb15b
Aaron <Aaron@As-MacBook-Pro.local>
2020-06-19 11:08:00 -0700

    completed course list

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import CourseItem from './components/CourseItem';
 import Button from './components/Button';
 import Header from './components/Header';
 import CourseOverview from './components/courseOverview';
+import CourseItemList from './components/CourseItemList';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Button />
+      <CourseItemList />
       <CourseOverview />
       <CourseItem />
       <ReviewItemList />
