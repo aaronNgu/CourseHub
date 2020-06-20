b0d6f5bc079d6660addf96fa373514bf092f4750
Illean Zhang <illean@outlook.com>
2020-06-19 18:48:27 -0700

    fixed header styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,18 +4,20 @@ import "./Header.css"
 
 class Header extends React.Component {
 
-  render() {
-    return (
-        <header>
-            <ul className="navBar">
-                <li><Link to="/homepage" className="link">CourseHub</Link></li>
-                <li><Link to="/coursepage" className="link">Temp. Course Page</Link></li>
-                <li><Link to="/about" className="link">About</Link></li>
-                <li><Link to="/contact" className="link">Contact </Link></li>
-            </ul>
-        </header>
-    );
-  }
+    render() {
+        return (
+            <header>
+                    <ul className="navBar">
+                        <li><Link to="/homepage" className="link home">CourseHub</Link></li>
+                        <div className="right">
+                        <li><Link to="/coursepage" className="link right">Temp. Course Page</Link></li>
+                        <li><Link to="/about" className="link right">About</Link></li>
+                        <li><Link to="/contact" className="link right">Contact </Link></li>
+                        </div>
+                    </ul>
+            </header>
+        );
+    }
 }
 
 export default Header;
