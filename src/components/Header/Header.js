7ecd3afd0ac8dd8951cc92954c06c980931e3aa4
Aaron <Aaron@As-MacBook-Pro.local>
2020-06-22 21:15:27 -0700

    refactor header to use flexbox for more responsiveness

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 import "./Header.css"
 
 class Header extends React.Component {
@@ -7,14 +7,30 @@ class Header extends React.Component {
     render() {
         return (
             <header>
-                    <ul className="navBar">
-                        <li><Link to="/homepage" className="link home">CourseHub</Link></li>
-                        <div className="right">
-                        <li><Link to="/coursepage" className="link right">Temp. Course Page</Link></li>
-                        <li><Link to="/about" className="link right">About</Link></li>
-                        <li><Link to="/contact" className="link right">Contact </Link></li>
-                        </div>
-                    </ul>
+                <ul className="navBar">
+                    <li>
+                        <Link to="/homepage" className="link home">
+                            CourseHub
+                        </Link>
+                    </li>
+                    <div className="right">
+                        <li>
+                            <Link to="/coursepage" className="link right">
+                                Temp. Course Page
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/about" className="link right">
+                                About
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/contact" className="link right">
+                                Contact
+                            </Link>
+                        </li>
+                    </div>
+                </ul>
             </header>
         );
     }
