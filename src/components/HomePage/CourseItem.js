3b1d3627e607deb7875ce8fef1749d8f6c7d38be
Aaron <Aaron@As-MacBook-Pro.local>
2020-06-23 20:17:12 -0700

    removed Icons from Homepage

--- src/components/HomePage/CourseItem.js
+++ src/components/HomePage/CourseItem.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography, Paper, Box } from '@material-ui/core';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
-import LooksOneIcon from '@material-ui/icons/LooksOne';
-import LooksTwoIcon from '@material-ui/icons/LooksTwo';
-import Looks3Icon from '@material-ui/icons/Looks3';
-import Looks4Icon from '@material-ui/icons/Looks4';
-import Looks5Icon from '@material-ui/icons/Looks5';
 import "./HomePage.css"
 
 const useStyles = makeStyles({
@@ -34,7 +27,7 @@ function CourseItem(props) {
         <Box className='courseItemHorizontal'>
 
             <Box className='courseItemVerticalSides'>
-                <Typography variant='h6'>Rating</Typography>
+                <Typography variant='h6'>&nbsp;Rating</Typography>
                 <Paper className={classes.rating}>
                     <Typography variant='h3'>{props.rating || 5}</Typography>
                 </Paper>
@@ -49,21 +42,6 @@ function CourseItem(props) {
 
         <Box className='courseItemVerticalSides courseItemVerticalMiddle'>
             <Typography variant='body2'>{props.date || '2/19/2020'}</Typography>
-            <Box className='courseItemIconContainer'>
-                <LooksOneIcon className='courseItemIcon' onClick={() => props.onRatingOne(props.courseNumber)}/>
-                &nbsp;&nbsp;
-                <LooksTwoIcon className='courseItemIcon' onClick={() => props.onRatingTwo(props.courseNumber)}/>
-                &nbsp;&nbsp;
-                <Looks3Icon className='courseItemIcon' onClick={() => props.onRatingThree(props.courseNumber)}/>
-                &nbsp;&nbsp;
-                <Looks4Icon className='courseItemIcon' onClick={() => props.onRatingFour(props.courseNumber)}/>
-                &nbsp;&nbsp;
-                <Looks5Icon className='courseItemIcon' onClick={() => props.onRatingFive(props.courseNumber)}/>
-                &nbsp;&nbsp;
-                <EditIcon className='courseItemIcon' onClick={() => props.onEdit(props.courseNumber)}/>
-                &nbsp;&nbsp;
-                <DeleteIcon className='courseItemIcon' onClick={() => props.onCancel(props.courseNumber)}/>
-            </Box>
         </Box>
 
     </Box>;
--- src/components/HomePage/CourseItemList.js
+++ src/components/HomePage/CourseItemList.js
@@ -1,44 +1,9 @@
 import React from 'react';
 import CourseItem from './CourseItem';
 import {connect} from 'react-redux';
-import {deleteMessage, deleteAllMessages, editMessage, editRating1, editRating2,
-   editRating3, editRating4, editRating5} from '../../actions';
 import Button from "@material-ui/core/Button";
 
 class CourseItemList extends React.Component {
-
-    handleCancel = (course) => {
-        this.props.deleteMessage(course);
-    }
-
-    handleEdit = (course) => {
-        this.props.editMessage(course);
-    }
-
-    handleDeleteAll = (courseList) => {
-        this.props.deleteAllMessages(courseList);
-    }
-
-    handleRatingOne = (course) => {
-        this.props.editRating1(course);
-    }
-
-    handleRatingTwo = (course) => {
-        this.props.editRating2(course);
-    }
-
-    handleRatingThree = (course) => {
-        this.props.editRating3(course);
-    }
-
-    handleRatingFour = (course) => {
-        this.props.editRating4(course);
-    }
-
-    handleRatingFive = (course) => {
-        this.props.editRating5(course);
-    }
-
 
     render() {
         return (<div className="reviewedCourses">
@@ -57,19 +22,9 @@ class CourseItemList extends React.Component {
                             courseNumber={course}
                             rating={this.props.courseList[course]['summary']['rating']}
                             review={this.props.courseList[course]['reviews']['1']['comment']}
-                            onRatingOne={this.handleRatingOne}
-                            onRatingTwo={this.handleRatingTwo}
-                            onRatingThree={this.handleRatingThree}
-                            onRatingFour={this.handleRatingFour}
-                            onRatingFive={this.handleRatingFive}
-                            onEdit={this.handleEdit}
-                            onCancel={this.handleCancel}
                         />;
                     })
                 }
-                <Button variant="outlined" onClick={this.handleDeleteAll}>
-                Delete all courses
-                </Button>
             </div>
         </div>);
     }
@@ -80,6 +35,4 @@ const mapStateToProps = (state) => {
     return {courseList: state.courseList};
 }
 
-export default connect(mapStateToProps, {deleteMessage, deleteAllMessages,
-  editMessage, editRating1, editRating2, editRating3, editRating4, editRating5})
-  (CourseItemList);
+export default connect(mapStateToProps)(CourseItemList);
