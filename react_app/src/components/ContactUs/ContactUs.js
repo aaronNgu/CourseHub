8123229014207ab1bc3a74dbea0f7087e257c182
Illean Zhang <illean@outlook.com>
2020-06-29 21:56:19 -0700

    gah im stupid forgot to format before committing

--- react_app/src/components/ContactUs/ContactUs.js
+++ react_app/src/components/ContactUs/ContactUs.js
@@ -4,7 +4,7 @@ import "./ContactUs.css"
 import TextField from '@material-ui/core/TextField';
 import Button from "@material-ui/core/Button";
 import Icon from "@material-ui/core/Icon";
-import {makeStyles, withStyles} from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
     button: {
@@ -18,35 +18,34 @@ const useStyles = makeStyles({
 function ContactPage(props) {
     const classes = useStyles();
 
-        return             <div className="contactPage">
-                <h1>Contact Us!</h1>
-                <div className="contactForm">
-                <TextField
-                    required
-                    label="Subject"
-                    className="subject"
-                    margin="normal"
-                />
-                <TextField
-                    required
-                    label="Message"
-                    multiline
-                    rowsMax="5"
-                    className="message"
-                    margin="normal"
-                />
-                <Button
-                    variant="contained"
-                    color="#A17070"
-                    className={classes.button}
-                    id="sendButton"
-                    >
-                    Send
-                    <Icon>send</Icon>
-                </Button>
-                </div>
-            </div>
-        ;
-};
+    return <div className="contactPage">
+        <h1>Contact Us!</h1>
+        <div className="contactForm">
+            <TextField
+                required
+                label="Subject"
+                className="subject"
+                margin="normal"
+            />
+            <TextField
+                required
+                label="Message"
+                multiline
+                rowsMax="5"
+                className="message"
+                margin="normal"
+            />
+            <Button
+                variant="contained"
+                color="#A17070"
+                className={classes.button}
+                id="sendButton"
+            >
+                Send
+                <Icon>send</Icon>
+            </Button>
+        </div>
+    </div>;
+}
 
 export default ContactPage;
