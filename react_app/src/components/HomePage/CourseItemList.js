00e7912beafe47224c0fffc745a81664e312d0b7
Illean Zhang <illean@outlook.com>
2020-07-02 22:12:16 -0700

    search bar working yay

--- react_app/src/components/HomePage/CourseItemList.js
+++ react_app/src/components/HomePage/CourseItemList.js
@@ -5,8 +5,20 @@ import {connect} from 'react-redux';
 import {deleteMessage, deleteAllMessages, editMessage, editRating1, editRating2,
    editRating3, editRating4, editRating5, fetchCourses} from '../../actions';
 import Button from "@material-ui/core/Button";
+import InputBase from "@material-ui/core/InputBase";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from '@material-ui/icons/Search';
 
 class CourseItemList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {searchString: ''};
+    }
+
+    handleChange = (e) => {
+        this.setState({searchString: e.target.value});
+    }
 
   componentDidMount() {
           this.props.dispatch(fetchCourses());
@@ -17,13 +29,24 @@ class CourseItemList extends React.Component {
         return (<div className="reviewedCourses">
             <div className="courseListHeader content">
                 <p>Reviewed Courses</p>
+                <div className="searchBar">
+                        <InputBase
+                            placeholder="Search"
+                            onChange={this.handleChange}
+                        />
+                        <IconButton type="submit" aria-label="search">
+                            <SearchIcon />
+                        </IconButton>
+                </div>
                 <div className="button">
                     <Button variant="outlined">Filter</Button>
                 </div>
             </div>
             <div className="courseList">
                 {
-                    Object.values(this.props.courseList).map((course, index) => {
+                    Object.values(this.props.courseList)
+                        .filter(course => course._id.toString().toLowerCase().includes(this.state.searchString.toLowerCase()))
+                        .map((course, index) => {
                         return <CourseItem
                             key={index}
                             courseNumber={course._id}
@@ -41,7 +64,10 @@ class CourseItemList extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-    return {courseList: state.courseList};
+    return {
+        courseList: state.courseList,
+        searchString: state.searchString
+    };
 }
 
 export default connect(mapStateToProps)(CourseItemList);
