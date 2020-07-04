77e190ab87cfb8654444aed28cd26dab027139e3
Illean Zhang <illean@outlook.com>
2020-07-03 20:21:14 -0700

    finally got filter working.... i am a disappointment

--- react_app/src/components/HomePage/CourseItemList.js
+++ react_app/src/components/HomePage/CourseItemList.js
@@ -2,27 +2,43 @@ import React from 'react';
 import CourseItem from './CourseItem';
 import AddCourseFormDialog from './AddCourseFormDialog';
 import {connect} from 'react-redux';
-import {deleteMessage, deleteAllMessages, editMessage, editRating1, editRating2,
-   editRating3, editRating4, editRating5, fetchCourses} from '../../actions';
+import {fetchCourses} from '../../actions';
 import Button from "@material-ui/core/Button";
 import InputBase from "@material-ui/core/InputBase";
 import IconButton from "@material-ui/core/IconButton";
 import SearchIcon from '@material-ui/icons/Search';
+import Filters from "./Filters";
 
 class CourseItemList extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {searchString: ''};
+        this.state = {
+            searchString: '',
+            showFilters: false,
+            yearLvFilter: ['100', '200', '300', '400', '500', '600'],
+            ratingFilter: ['1', '2', '3', '4', '5', '*']
+        };
+        this.onFiltersUpdate = this.onFiltersUpdate.bind(this);
     }
 
     handleChange = (e) => {
         this.setState({searchString: e.target.value});
     }
 
-  componentDidMount() {
-          this.props.dispatch(fetchCourses());
-      };
+    componentDidMount() {
+        this.props.dispatch(fetchCourses());
+    };
+
+    toggleFilter() {
+        this.setState({
+            showFilters: !this.state.showFilters
+        });
+    }
+
+    onFiltersUpdate(yearLvFilter, ratingFilter) {
+        this.setState({...this.state, yearLvFilter, ratingFilter});
+    }
 
     render() {
 
@@ -30,34 +46,41 @@ class CourseItemList extends React.Component {
             <div className="courseListHeader content">
                 <p>Reviewed Courses</p>
                 <div className="searchBar">
-                        <InputBase
-                            placeholder="Search"
-                            onChange={this.handleChange}
-                        />
-                        <IconButton type="submit" aria-label="search">
-                            <SearchIcon />
-                        </IconButton>
+                    <InputBase
+                        placeholder="Search"
+                        onChange={this.handleChange}
+                    />
+                    <IconButton type="submit" aria-label="search">
+                        <SearchIcon/>
+                    </IconButton>
                 </div>
                 <div className="button">
-                    <Button variant="outlined">Filter</Button>
+                    <Button variant="outlined"
+                            onClick={this.toggleFilter.bind(this)}
+                    >Filter</Button>
                 </div>
             </div>
+            {this.state.showFilters ? (<Filters yearLvFilter={this.state.yearLvFilter}
+                                                ratingFilter={this.state.ratingFilter}
+                                                updateFilters={this.onFiltersUpdate}/>) : null}
             <div className="courseList">
                 {
                     Object.values(this.props.courseList)
                         .filter(course => course._id.toString().toLowerCase().includes(this.state.searchString.toLowerCase()))
+                        .filter(course => this.state.yearLvFilter.includes((course._id.toString().slice(4, 5) + '00'))
+                            && this.state.ratingFilter.includes(course.overall_rating.toString()))
                         .map((course, index) => {
-                        return <CourseItem
-                            key={index}
-                            courseNumber={course._id}
-                            rating={course.overall_rating}
-                            review={course.description}
-                        />;
-                    })
+                            return <CourseItem
+                                key={index}
+                                courseNumber={course._id}
+                                rating={course.overall_rating}
+                                review={course.description}
+                            />;
+                        })
                 }
             </div>
             <div>
-            <AddCourseFormDialog/>
+                <AddCourseFormDialog/>
             </div>
         </div>);
     }
@@ -66,7 +89,10 @@ class CourseItemList extends React.Component {
 const mapStateToProps = (state) => {
     return {
         courseList: state.courseList,
-        searchString: state.searchString
+        searchString: state.searchString,
+        showFilters: state.showFilters,
+        yearLvFilter: state.yearLvFilter,
+        ratingFilter: state.ratingFilter,
     };
 }
 
---
+++ react_app/src/components/HomePage/Filters.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import {Box, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel} from '@material-ui/core';
+import {connect} from 'react-redux'
+import "./HomePage.css"
+import {update_filters} from '../../actions';
+
+const yearLvList = ['100', '200', '300', '400', '500', '600'];
+const ratingList = ['1', '2', '3', '4', '5', '*'];
+
+class Filters extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            yearLvFilter: this.props.yearLvFilter,
+            ratingFilter: this.props.ratingFilter,
+        };
+    }
+
+    handleYearLvChange = (e) => {
+        let newState = this.state;
+        if (this.state.yearLvFilter.includes(e.target.name)) {
+            newState = {
+                ...this.state, yearLvFilter: this.state.yearLvFilter.filter((year) => {
+                    return year !== e.target.name
+                })
+            };
+        } else {
+            newState = {...this.state, yearLvFilter: [...this.state.yearLvFilter, e.target.name]};
+        }
+        this.setState(newState);
+        this.handleFilterUpdate(newState);
+    }
+
+    handleRatingChange = (e) => {
+        let newState = this.state;
+        if (this.state.ratingFilter.includes(e.target.name)) {
+            newState = {
+                ...this.state, ratingFilter: this.state.ratingFilter.filter((rating) => {
+                    return rating !== e.target.name
+                })
+            };
+        } else {
+            newState = {...this.state, ratingFilter: [...this.state.ratingFilter, e.target.name]};
+        }
+        this.setState(newState);
+        this.handleFilterUpdate(newState);
+    }
+
+    handleFilterUpdate = (newState) => {
+        this.props.update_filters(newState.yearLvFilter, newState.ratingFilter);
+        this.props.updateFilters(newState.yearLvFilter, newState.ratingFilter);
+    }
+
+    render() {
+        return (
+            <Box className="filterContainer">
+                <div className="filterCategories">
+                    <FormControl className="yearLevelFilter">
+                        <FormLabel>Year Level:</FormLabel>
+                        <FormGroup row>
+                            {
+                                yearLvList.map((year) => {
+                                    return <FormControlLabel
+                                        control={<Checkbox defaultChecked
+                                                           style={{color: '#E5A0A0',}}
+                                                           onChange={this.handleYearLvChange}
+                                                           name={year}/>}
+                                        label={year}
+                                    />
+                                })
+                            }
+                        </FormGroup>
+                    </FormControl>
+                    <FormControl className="ratingFilter">
+                        <FormLabel>Rating:</FormLabel>
+                        <FormGroup row>
+                            {
+                                ratingList.map((rating) => {
+                                    return <FormControlLabel
+                                        control={<Checkbox defaultChecked
+                                                           style={{color: '#E5A0A0',}}
+                                                           onChange={this.handleRatingChange}
+                                                           name={rating}/>}
+                                        label={rating}
+                                    />
+                                })
+                            }
+                        </FormGroup>
+                    </FormControl>
+                </div>
+            </Box>
+        );
+    }
+}
+
+export default connect(
+    null,
+    {update_filters}
+)(Filters);
