3b18def71120680b21de3b948fcf4c5647d5fbe2
Aaron <Aaron@As-MacBook-Pro.local>
2020-07-06 20:53:25 -0700

    add dotenv package and integrated the usage of dotenv

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,22 +12,22 @@ var coursesRouter = require('./routes/courses');
 var reviewsRouter = require('./routes/reviews');
 
 var app = express();
-app.use(cors());
 
 //connect to mongo db
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://m001-student:m001-mongodb-basics@sandbox-7vuqw.mongodb.net/courseHub?retryWrites=true&w=majority')
+const mongoose = require('mongoose');
+const config = process.env;
+mongoose.connect('mongodb+srv://' + config.DB_USER  + ':' + config.DB_PW + '@sandbox-7vuqw.mongodb.net/' + config.DB_DBNAME + '?retryWrites=true&w=majority')
   .then( () => {
     console.log('Connection to the Atlas Cluster is successful!')
   })
   .catch( (err) => console.error(err));
 
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
