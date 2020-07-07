70f06303b075056f6c5b9ba41697e6f2105bd8c5
Aaron <Aaron@As-MacBook-Pro.local>
2020-07-07 16:02:06 -0700

    completed LoginPage

--- react_app/src/components/LoginPage/LoginPage.js
+++ react_app/src/components/LoginPage/LoginPage.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import './LoginPage.css';
+import LoginPageBox from './LoginPageBox';
 
 class LoginPage extends React.Component {
 
     render() {
-        return (<div>
-            <Typography variant='h5'>Log in to rate a course</Typography>
-            <Typography variant='body2'>
-                Don't have an account? Click <Link to="#" >here</Link> to sign up
-            </Typography>
+        return (<div className="LoginPageMain">
+            <LoginPageBox/>
         </div>)
     }
 }
