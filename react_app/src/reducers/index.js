9d41dff3cd9b3f49fb9f1e4af69cb52bdd3d87e0
Aaron <Aaron@As-MacBook-Pro.local>
2020-07-07 20:02:29 -0700

    some refactoring

--- react_app/src/reducers/index.js
+++ react_app/src/reducers/index.js
@@ -4,9 +4,7 @@ import thunk from 'redux-thunk';
 const yearLvFilter = ['100', '200', '300', '400', '500', '600'];
 const ratingFilter = ['1', '2', '3', '4', '5', '*'];
 
-let default_data = {}
-
-const courseReducer = (courseList = default_data, action) => {
+const courseReducer = (courseList = {}, action) => {
     //TODO: remove irrelevant reducers
     if (action.type === 'DELETE_MESSAGE') {
         let copy = Object.assign({}, courseList);
@@ -16,31 +14,6 @@ const courseReducer = (courseList = default_data, action) => {
     if (action.type === 'DELETE_ALL_MESSAGES') {
         courseList = {};
         return courseList;
-    }
-    if (action.type === 'EDIT_RATING_1') {
-        let copy = Object.assign({}, courseList);
-        copy[action.payload].summary.rating = 1;
-        return copy;
-    }
-    if (action.type === 'EDIT_RATING_2') {
-        let copy = Object.assign({}, courseList);
-        copy[action.payload].summary.rating = 2;
-        return copy;
-    }
-    if (action.type === 'EDIT_RATING_3') {
-        let copy = Object.assign({}, courseList);
-        copy[action.payload].summary.rating = 3;
-        return copy;
-    }
-    if (action.type === 'EDIT_RATING_4') {
-        let copy = Object.assign({}, courseList);
-        copy[action.payload].summary.rating = 4;
-        return copy;
-    }
-    if (action.type === 'EDIT_RATING_5') {
-        let copy = Object.assign({}, courseList);
-        copy[action.payload].summary.rating = 5;
-        return copy;
     }
     if (action.type === 'FETCHED_COURSES') {
         return Object.assign({}, courseList,
