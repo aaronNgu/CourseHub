9bdbf170786bb67fb41e2cbe8782318bd013d37d
Aaron <Aaron@As-MacBook-Pro.local>
2020-07-09 15:23:52 -0700

    courseOverview uses refactored component

--- react_app/src/components/CoursePage/courseOverview.js
+++ react_app/src/components/CoursePage/courseOverview.js
@@ -1,33 +1,50 @@
 import React from 'react';
 import "./CoursePage.css"
-import { Typography, Box} from '@material-ui/core';
+import { Typography, Box } from '@material-ui/core';
 import Rating from './Rating';
 import RateCourseButton from './RateCourseButton';
+import {connect} from 'react-redux';
 
-function CourseOverview() {
-    // TODO: add onclick -> rating input page
-    return <Box className='courseOverview content'>
+class CourseOverview extends React.Component {
+    
+    handleRateCourse = () => {
+        const auth = this.props.auth.isAuthenticated;
+        console.log(auth);
+        if (auth) {
+            console.log('open rate my course dialog');
+        } else {
+            window.location.replace('/login');
+        }
+    }
 
-        <Box className='courseOverviewHeader'>
-            <Typography variant='h5'>{'CPSC110'} </Typography>
-            <Typography variant='h5'>{'Computations, Programs, and Programming'}</Typography>
-            <RateCourseButton/>
-        </Box>
+    render() {
+        return <Box className='courseOverview content'>
 
-        <Box className='courseOverviewHorizontal'>
-            <Box className='courseOverviewVerticalLeft'>
-                <Typography variant='h6'
-                    style={{ whiteSpace: 'pre-line' }}>{'Overall \n Rating'}</Typography>
-                <Rating rating='5' />
+            <Box className='courseOverviewHeader'>
+                <Typography variant='h5'>{'CPSC110'} </Typography>
+                <Typography variant='h5'>{'Computations, Programs, and Programming'}</Typography>
+                <RateCourseButton handleRateCourse={this.handleRateCourse}/>
             </Box>
 
-            <Box className='courseOverviewVerticalRight'>
-                <Typography variant='h6'>{'Top Review'} </Typography>
-                <Typography variant='body2'>{'It was an awesome course!'}</Typography>
-            </Box>
-        </Box>
+            <Box className='courseOverviewHorizontal'>
+                <Box className='courseOverviewVerticalLeft'>
+                    <Typography variant='h6'
+                        style={{ whiteSpace: 'pre-line' }}>{'Overall \n Rating'}</Typography>
+                    <Rating rating='5' />
+                </Box>
 
-    </Box>;
+                <Box className='courseOverviewVerticalRight'>
+                    <Typography variant='h6'>{'Top Review'} </Typography>
+                    <Typography variant='body2'>{'It was an awesome course!'}</Typography>
+                </Box>
+            </Box>
+
+        </Box>;
+    }
 }
 
-export default CourseOverview;
+const mapStateToProps = (state) => {
+    return {auth: state.auth};
+}
+
+export default connect(mapStateToProps)(CourseOverview);
