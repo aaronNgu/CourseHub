db3fb3c2c1001a8a32490186b5bde8d03cbe10bc
Aaron <Aaron@As-MacBook-Pro.local>
2020-07-10 11:22:28 -0700

    Facebook passport setup

---
+++ api/FacebookSetup.js
@@ -0,0 +1,21 @@
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
+require('dotenv').config();
+
+const fbcallback = (accessToken, refreshToken, profile, cb) => {
+	return cb(null, profile);
+};
+
+passport.use(new FacebookStrategy({
+	clientID: process.env.FB_CLIENT,
+	clientSecret: process.env.FB_SECRET,
+	callbackURL: '/auth/facebook/redirect',
+}, fbcallback));
+
+passport.serializeUser(function (user, cb) {
+	cb(null, user)
+});
+
+passport.deserializeUser(function (user, cb) {
+	cb(null, user)
+});
