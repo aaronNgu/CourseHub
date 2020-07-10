97e492d603306b9e4f666bdac667a65be1e68515
Aaron <Aaron@As-MacBook-Pro.local>
2020-07-10 11:28:38 -0700

    set up facebook router in app.js

--- api/app.js
+++ api/app.js
@@ -11,6 +11,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var coursesRouter = require('./routes/courses');
 var reviewsRouter = require('./routes/reviews');
+var facebookRoute = require('./routes/facebookAuth');
 
 var app = express();
 
@@ -39,6 +40,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/courses', coursesRouter);
 app.use('/reviews', reviewsRouter);
+app.use('/auth/facebook', facebookRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
