3da3062f2c831b771d1c405e718fcec319718583
Aaron <Aaron@As-MacBook-Pro.local>
2020-07-10 12:49:57 -0700

    added credentials and origin for cors

--- api/app.js
+++ api/app.js
@@ -38,7 +38,7 @@ app.use(cookieSession({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(cors({credentials: true,}));
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
