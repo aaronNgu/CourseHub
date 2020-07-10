66d8a4a1fbff9d218d4687e2e7ff303677d883fd
Aaron <Aaron@As-MacBook-Pro.local>
2020-07-10 13:38:58 -0700

    make TestBox a controlled compoenent

--- react_app/src/components/CoursePage/TextBox.js
+++ react_app/src/components/CoursePage/TextBox.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core';
+import {connect} from 'react-redux';
+import {addReviewReview} from '../../actions';
 
 const useStyles = makeStyles({
     root: {
@@ -9,7 +11,7 @@ const useStyles = makeStyles({
     }
 });
 
-const TextBox = () => {
+const TextBox = ({review, addReviewReview}) => {
     const placeholderReview = "It was pretty fun course. No midterms. Awesome prof. However I wish there was less homewor."
     const classes = useStyles();
 
@@ -19,8 +21,10 @@ const TextBox = () => {
                 placeholder={placeholderReview}
                 multiline
                 rows={10}
+                value={review}
                 rowsMax={4}
                 color='secondary'
+                onChange={(event, newValue) => {addReviewReview(event.target.value)}}
                 inputProps={{
                     style: {
                         height: '200px',
@@ -28,4 +32,8 @@ const TextBox = () => {
                 }}/>
 }
 
-export default TextBox;
+const mapStateToProps = (state) => {
+    return {review: state.addReviewReview};
+}
+
+export default connect(mapStateToProps, {addReviewReview})(TextBox);
