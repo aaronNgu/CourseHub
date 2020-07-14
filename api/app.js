a3091b98c0c2393f3c4a38f0280964d7e7a335c5
Aaron <Aaron@As-MacBook-Pro.local>
2020-07-13 20:56:16 -0700

    remove more clutter

--- api/app.js
+++ api/app.js
@@ -2,7 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var bodyParser = require("body-parser");
 var logger = require('morgan');
 var cors = require('cors');
 require('dotenv').config();
@@ -68,10 +67,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
