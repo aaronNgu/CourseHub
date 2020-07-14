3c54d5ae55688c6ec4332f34dde2289a2f871dfa
Spandana Baruah <spandana.baruah@gmail.com>
2020-07-13 19:25:52 -0700

    added google client and secret to env file

--- api/google-setup.js
+++ api/google-setup.js
@@ -1,9 +1,10 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('dotenv').config();
 
 passport.use(new GoogleStrategy({
-    clientID: "798089317931-hca1vqdbgf41bcu3vnbsg4c7f1h58gdu.apps.googleusercontent.com",
-    clientSecret: "jQCNTrDmsy9sTwy8ANjtxCwB",
+    clientID: process.env.GOOGLE_CLIENT,
+    clientSecret: process.env.GOOGLE_SECRET,
     callbackURL: "/auth/google/redirect",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
