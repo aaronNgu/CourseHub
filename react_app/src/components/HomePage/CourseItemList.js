7e5ed14f9d59df7d8480960d4fe7218835a3122e
wchan <Because1215>
2020-07-21 15:09:14 -0400

    Remove reload

--- react_app/src/components/HomePage/CourseItemList.js
+++ react_app/src/components/HomePage/CourseItemList.js
@@ -28,7 +28,7 @@ class CourseItemList extends React.Component {
 
     componentDidMount() {
         this.props.dispatch(fetchCourses());
-          window.location.reload()
+        window.location.reload()
     };
 
     toggleFilter() {
