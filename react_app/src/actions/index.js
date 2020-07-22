c1101305702d41623ef3b36375589a9cbff6a99b
Aaron <Aaron@As-MacBook-Pro.local>
2020-07-21 19:41:12 -0700

    remove unused action

--- react_app/src/actions/index.js
+++ react_app/src/actions/index.js
@@ -1,56 +1,4 @@
 
-export const deleteMessage = (courseNumber) => {
-    return {
-        type: 'DELETE_MESSAGE',
-        payload: courseNumber
-    };
-}
-
-export const deleteAllMessages = (courseList) => {
-    return {
-        type: 'DELETE_ALL_MESSAGES',
-        payload: courseList
-    };
-}
-
-export const editMessage = (courseNumber) => {
-    return {
-        type: 'EDIT_MESSAGE',
-        payload: courseNumber
-    };
-}
-
-export const editRating1 = (courseNumber) => {
-    return {
-        type: 'EDIT_RATING_1',
-        payload: courseNumber
-    };
-}
-export const editRating2 = (courseNumber) => {
-    return {
-        type: 'EDIT_RATING_2',
-        payload: courseNumber
-    };
-}
-export const editRating3 = (courseNumber) => {
-    return {
-        type: 'EDIT_RATING_3',
-        payload: courseNumber
-    };
-}
-export const editRating4 = (courseNumber) => {
-    return {
-        type: 'EDIT_RATING_4',
-        payload: courseNumber
-    };
-}
-export const editRating5 = (courseNumber) => {
-    return {
-        type: 'EDIT_RATING_5',
-        payload: courseNumber
-    };
-}
-
 export const fetched_courses = courses => {
     return {
         type: "FETCHED_COURSES",
@@ -127,13 +75,6 @@ export const update_filters = (yearLvFilter, ratingFilter) => {
     };
 };
 
-export const authenticated = (payload) => {
-    return {
-        type: 'AUTH',
-        payload: payload,
-    }
-}
-
 export const toggleAddReviewBox = (payload) => {
     return {
         type: 'TOGGLE',
@@ -154,6 +95,25 @@ export const addReviewReview = (payload) => {
         payload: payload,
     }
 }
+
+export const fetchReviews = (courseId) => {
+  return function(dispatch, getState) {
+    return fetch(`/reviews/course/`+courseId)
+      .then(
+				data => data.json())
+      .then(data => {
+					dispatch(fetched_reviews(data))}
+      )
+      .catch(err => console.log(err));
+  };
+};
+
+export const fetched_reviews = reviews => {
+  return {
+    type: "FETCHED_REVIEWS",
+    data: reviews
+  };
+};
 
 export const checkStatus = () => {
     return function(dispatch , getState) {
@@ -188,21 +148,10 @@ export const logout = () => {
         })
     }
 }
-export const fetchReviews = (courseId) => {
-  return function(dispatch, getState) {
-    return fetch(`/reviews/course/`+courseId)
-      .then(
-				data => data.json())
-      .then(data => {
-					dispatch(fetched_reviews(data))}
-      )
-      .catch(err => console.log(err));
-  };
-};
 
-export const fetched_reviews = reviews => {
-  return {
-    type: "FETCHED_REVIEWS",
-    data: reviews
-  };
-};
+export const authenticated = (payload) => {
+    return {
+        type: 'AUTH',
+        payload: payload,
+    }
+}
