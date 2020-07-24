72eab711bc56020baf5ba19ea139028659fb2c0d
Illean Zhang <illean@outlook.com>
2020-07-23 21:28:14 -0700

    removed dotenv

--- app.js
+++ app.js
@@ -29,24 +29,24 @@ var app = express();
 //connect to mongo db
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://' + config.DB_USER + ':' + config.DB_PW + '@sandbox-7vuqw.mongodb.net/' + config.DB_DBNAME + '?retryWrites=true&w=majority', { useNewUrlParser: true })
-  .then(() => {
-    console.log('Connection to the Atlas Cluster is successful!')
-  })
-  .catch((err) => console.error(err));
+    .then(() => {
+        console.log('Connection to the Atlas Cluster is successful!')
+    })
+    .catch((err) => console.error(err));
 
 passport.serializeUser((user, done) => {
-  done(null, user);
+    done(null, user);
 });
 
 // deserialize the cookieUserId to user in the database
 passport.deserializeUser((user, done) => {
-  User.findById(user._id)
-    .then(user => {
-      done(null, user);
-    })
-    .catch(e => {
-      done(new Error("Failed to deserialize an user"));
-    });
+    User.findById(user._id)
+        .then(user => {
+            done(null, user);
+        })
+        .catch(e => {
+            done(new Error("Failed to deserialize an user"));
+        });
 });
 
 // view engine setup
@@ -54,11 +54,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(
-  cookieSession({
-    name: "session",
-    keys: ["thisappisawesome"],
-    maxAge: 24 * 60 * 60 * 100
-  })
+    cookieSession({
+        name: "session",
+        keys: ["thisappisawesome"],
+        maxAge: 24 * 60 * 60 * 100
+    })
 );
 app.use(cors({origin: config.CLIENT_BASE_URL, credentials: true}));
 app.use(logger('dev'));
@@ -83,18 +83,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 app.get('*', (req5,res1) =>{
