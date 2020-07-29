36b3549d1c4ccc6381d6bc3c9551fb206e8a729d
Aaron <Aaron@As-MacBook-Pro.local>
2020-07-28 22:20:41 -0700

    added search bar component

---
+++ react_app/src/components/HomePage/SearchBar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import SearchIcon from "@material-ui/icons/Search";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+        '& .Mui-focused': {
+            color: 'white',
+          }, 
+        '& .MuiOutlinedInput-root': {
+            '&.Mui-focused fieldset': {
+                borderColor: 'black',
+            },
+        }
+    },
+    input:  {
+        background: 'white',
+    }
+});
+
+const SearchBar = () => {
+    const classes = useStyles();
+    return (<div className='searchBox content'>
+        <div>
+            <Typography variant="h5"> Find a course</ Typography>
+            <TextField
+                variant='outlined'
+                className={classes.root}
+                InputProps={{
+                    className: classes.input,
+                    endAdornment: (
+                        <InputAdornment>
+                            <IconButton>
+                                <SearchIcon />
+                            </IconButton>
+                        </InputAdornment>
+                    )
+                }}></TextField>
+        </div>
+    </div>);
+}
+
+export default SearchBar; 
