65fcf9ebae3f1e67231b56cd3f6824381ec4375b
Aaron <Aaron@As-MacBook-Pro.local>
2020-08-02 12:19:46 -0700

    fix paginaiton bug on the backend

--- routes/search.js
+++ routes/search.js
@@ -13,7 +13,7 @@ const searchHelper = (input, req, res) => {
     .aggregate([result])
     .exec()
     .then(docs => {
-        const pagenumber = 'page' in input ? 0 : (req.query.page - 1 );
+        const pagenumber = 'page' in input ? (input['page'] - 1 ) : 0 ;
         let pageCount = getNumberOfPages(docs);
         let courses = get10NthFromStart(docs, pagenumber);
         const response = {
