04b00d8348deed5828af07fd3448d07ce81cf373
Aaron <Aaron@As-MacBook-Pro.local>
2020-08-02 22:44:18 -0700

    completed update rating

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const mongoose = require('mongoose')
-const Review = require('../models/review')
+const mongoose = require('mongoose');
+const Review = require('../models/review');
+const Course = require('../models/course');
 
 /* GET Reviews listing. */
 router.get('/', function (req, res, next) {
@@ -47,6 +48,37 @@ router.get('/course/:courseId', function (req, res, next) {
   });
 });
 
+const calculateNewRating = (numOfRatings, overallRating, newRating) => {
+  if (overallRating === '-' || overallRating === '0') {
+    return newRating;
+  }
+  let total = parseInt(overallRating) * numOfRatings;
+  total += parseInt(newRating);
+  numOfRatings += 1;
+  let average = total / numOfRatings++;
+  average = Math.round(average);
+  return average.toString();
+};
+
+const updateCourseInformation = (courseId, newRating) => {
+  Course
+  .findById(courseId) 
+  .then(doc => {
+    const newAverage = calculateNewRating(doc['num_reviews'], doc['overall_rating'], newRating);
+    Course
+    .findByIdAndUpdate(courseId, 
+      {$inc : {'num_reviews' : 1}, 'overall_rating': newAverage},
+      {new: 1})
+    .then(updatedCourse => {
+      // console.log(updatedCourse);
+    })
+    .catch(err => {
+      console.log('Failed to update Course Info.');
+      console.log(err);
+    })
+  })
+};
+
 router.post('/', function(req, res, next) {
   if(req.user){
     const newReview = new Review({
@@ -58,7 +90,9 @@ router.post('/', function(req, res, next) {
     });
 
     newReview.save()
-    .then(result => {res.status(200).json(result);})
+    .then(result => {
+      updateCourseInformation(req.body.Course_id, req.body.Rating);
+      res.status(200).json(result);})
     .catch(err => {
       console.log(err);
       res.status(500).json({
