fca4f190a3d8b0c81a1ed7ba0c141e15c919c387
Illean Zhang <illean@outlook.com>
2020-08-04 23:08:07 -0700

    cleaned up the dispatches

--- react_app/src/actions/index.js
+++ react_app/src/actions/index.js
@@ -186,6 +186,13 @@ export const addReview = (review, rating, courseId) => {
     }
 }
 
+export const fetchCourseOverview = (courseId) => {
+    return function(dispatch, getState) {
+        dispatch(fetchCourseInfo(courseId));
+        dispatch(fetchReviews(courseId));
+    }
+}
+
 /* Authentication Action */
 export const checkStatus = () => {
     return function(dispatch , getState) {
