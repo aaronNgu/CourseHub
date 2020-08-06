8e5e27ab3b1f86300f417ce3b85c6c26842f17b7
Aaron <Aaron@As-MacBook-Pro.local>
2020-08-05 20:20:17 -0700

    remove unused component

--- react_app/src/components/CoursePage/ReviewItemList.js
+++ react_app/src/components/CoursePage/ReviewItemList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReviewItem from "./ReviewItem";
 import {connect} from 'react-redux';
-import Button from "@material-ui/core/Button";
 import {fetchReviews} from '../../actions';
 
 class ReviewItemList extends React.Component {
--- react_app/src/components/LoginPage/FacebookLoginButton.js
+++ react_app/src/components/LoginPage/FacebookLoginButton.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
-import FacebookIcon from '@material-ui/icons/Facebook';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
