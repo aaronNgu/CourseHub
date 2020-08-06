1ff93650035e39905b7b4ea81d42ca57e805b692
Spandana Baruah <spandana.baruah@gmail.com>
2020-08-06 12:34:57 -0700

    send email to admin gmail from customer add course form

--- react_app/src/components/HomePage/AddCourseFormDialog.js
+++ react_app/src/components/HomePage/AddCourseFormDialog.js
@@ -8,37 +8,54 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import {connect} from 'react-redux';
 import {addCourse} from '../../actions';
-
+import * as emailjs from "emailjs-com";
 function AddCourseFormDialog(props) {
   const [open, setOpen] = React.useState(false);
   let courseName = '';
   let courseDesc = '';
   let userRole = props.auth.userRole;
-
   const handleClickOpen = () => {
     setOpen(true);
   };
-
   const handleClose = () => {
     setOpen(false);
   };
-
-  const handleAdminSubmit = (courseName, courseDescription) => {
-    if (courseName === "" || courseDescription === "") {
+  const handleAdminSubmit = (courseName, courseDesc) => {
+    if (courseName === "" || courseDesc === "") {
       window.alert('Please do not leave the CourseName and Description fields empty.');
       return;
     } else {
-      props.dispatch(addCourse(courseName, courseDescription));
+      props.dispatch(addCourse(courseName, courseDesc));
       setOpen(false);
       window.location.reload();
     }
   };
-
-  const handleCustomerSubmit = (courseName, courseDescription) => {
-    setOpen(false);
-    window.alert('TODO: send email with Add-course info to admins.')
+  const handleCustomerSubmit = (courseName, courseDesc) => {
+    if (courseName === "" || courseDesc === "") {
+      window.alert('Please do not leave the CourseName and Description fields empty.');
+      return;
+    } else {
+      setOpen(false);
+      const templateId = 'add_course_dialog';
+      let template_params = {
+          "courseName": courseName,
+          "courseDesc": courseDesc
+      }
+      emailjs.send(
+        'gmail',
+        templateId,
+        template_params,
+        'user_8QyG7S5P1gOgQfcPrzqh5'
+        ).then(res => {
+          console.log(res);
+          window.alert('Add Course request successfully sent to Admins!');
+        })
+        .catch(err => {
+          console.log(err);
+          window.alert('Error! Add Course request was not sent to Admins!' + err);
+        })
+    }
   };
-
   return (
     <div>
       <Button variant="outlined" color="primary" onClick={handleClickOpen}>
@@ -47,7 +64,6 @@ function AddCourseFormDialog(props) {
       <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
         <DialogTitle id="form-dialog-title">Add Course</DialogTitle>
         <DialogContent>
-
             {userRole === 'Admin' ?
             <DialogContentText>
                 For Admins: To add course, please enter details below
@@ -55,7 +71,6 @@ function AddCourseFormDialog(props) {
             <DialogContentText>
                 Don't see the course you're looking for? Send us the course details and we'll add it for you!
             </DialogContentText>}
-
             <TextField
               autoFocus
               required
@@ -73,14 +88,11 @@ function AddCourseFormDialog(props) {
               onInput={ e=>courseDesc = e.target.value}
               fullWidth
             />
-
         </DialogContent>
         <DialogActions>
-
           <Button onClick={handleClose} color="primary">
             Cancel
           </Button>
-
           {userRole === 'Admin' ?
           <Button
             onClick={() => handleAdminSubmit(courseName, courseDesc)} color="primary">
@@ -90,19 +102,15 @@ function AddCourseFormDialog(props) {
             onClick={() => handleCustomerSubmit(courseName, courseDesc)} color="primary">
               Send to Admins
           </Button>}
-
         </DialogActions>
       </Dialog>
     </div>
   );
 }
-
-
 const mapStateToProps = (state) => {
     return {
       courseList: state.courseList,
       auth: state.auth
     }
 };
-
 export default connect(mapStateToProps)(AddCourseFormDialog);
